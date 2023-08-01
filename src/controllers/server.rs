94d64a0c46bcadee22700f46ce73fa7a82285e6c
elvisyong <elvisyong@gmail.com>
2023-08-01 11:11:46 +0800

    feat: add git ignore

--- src/controllers/server.rs
+++ src/controllers/server.rs
@@ -1,15 +1,10 @@
 use std::sync::Arc;
 
 use anyhow::Context;
-use axum::{
-    http::{
-        header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE, COOKIE},
-        Method,
-    },
-    Router,
-};
+use axum::Router;
 use tower::ServiceBuilder;
 use tower_http::cors::{self, CorsLayer};
+use tracing::log::info;
 
 use crate::{config::AppConfig, services::service_register::ServiceRegister};
 
@@ -30,19 +25,20 @@ pub async fn serve(config: Arc<AppConfig>) -> anyhow::Result<()> {
             // Read https://docs.rs/axum/latest/axum/middleware/index.html#ordering for more info
             ServiceBuilder::new().layer(
                 CorsLayer::new()
-                    .allow_credentials(true)
-                    .allow_methods([
-                        Method::GET,
-                        Method::POST,
-                        Method::OPTIONS,
-                        Method::DELETE,
-                        Method::PUT,
-                    ])
-                    .allow_origin(cors::Any) // In a real application, you should validate the `Origin` header.
-                    .allow_headers([AUTHORIZATION, ACCEPT, COOKIE, CONTENT_TYPE]),
+                    // .allow_credentials(true)
+                    // .allow_methods([
+                    //     Method::GET,
+                    //     Method::POST,
+                    //     Method::OPTIONS,
+                    //     Method::DELETE,
+                    //     Method::PUT,
+                    // ])
+                    // .allow_headers([AUTHORIZATION, ACCEPT, COOKIE, CONTENT_TYPE]),
+                    .allow_origin(cors::Any), // In a real application, you should validate the `Origin` header.
             ),
         );
 
+    info!("Starting server at {}", config.server_address);
     axum::Server::bind(&config.server_address.parse()?)
         .serve(app.into_make_service())
         .await
